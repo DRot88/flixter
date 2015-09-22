72a87f408f300df8312db24d957929adc974e6a6
Daniel Rotenberg <Daniel.Rotenberg88@gmail.com>
2015-09-22 00:07:39 -0400

    Installed devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  #devise for heroku
+  config.action_mailer.default_url_options = { host: 'flixter-daniel-rotenberg.herokuapp.com/'}
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
